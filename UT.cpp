b73f8fa6d26e76b90c27b7cead5936fdc5e177b4
RodrigoR3 <baudecoisas@gmail.com>
2018-10-09 09:37:43 -0300

    alterações iniciais

--- UT.cpp
+++ UT.cpp
@@ -1,13 +1,1 @@
-#include "vector" 
-
-Class UnidadeTransito {
-    int largura;
-    int comprimento;
-
-    float velocidade;
-    float aceleracao;
-
-    int comportamento;
-
-    float distancia_carro_adiante;
-}
+#include "UT.h" 
