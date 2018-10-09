24314fe13ad81fdd27f41c949bb43faaff603b04
RodrigoR3 <baudecoisas@gmail.com>
2018-10-09 08:56:44 -0300

    Unidade de Transito

---
+++ UT.cpp
@@ -0,0 +1,13 @@
+#include "vector" 
+
+Class UnidadeTransito {
+    int largura;
+    int comprimento;
+
+    float velocidade;
+    float aceleracao;
+
+    int comportamento;
+
+    float distancia_carro_adiante;
+}
